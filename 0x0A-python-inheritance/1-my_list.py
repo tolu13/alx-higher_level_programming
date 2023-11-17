18a545cf1c4e46eff1ad0c6ca3c670b89e08f2d2
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-11-17 03:53:05 +0000

    better late than never

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" It defines a subclass or child list class MYLIST   """
+
+class MyList(list)
+""" This is a subclass of class list """
+
+def print_sorted(self):
+        """Print a sorted list in a specific pattern."""
+        print(sorted(self))
