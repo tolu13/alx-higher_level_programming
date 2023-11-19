dd6b011750bbcbf4562b0078292a1d2f2603f615
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-11-19 14:35:06 +0000

    bbetter late than never

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this defines a class named BaseGeometry"""
+
+class BaseGeometry:
+	""" this is a BaseGeometry"""
+
+	def area(self):
+		""" Not Implemented"""
+		raise Exception("area() is not implemented")
