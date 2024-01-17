2e45a5f1e1a8cf36a27e4455c85c2deaf536fffd
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-01-17 02:15:31 -0800

    sql showed me shege

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-
-"""This script list all states
-from the database:hbtn_0e_0_usa
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
 from sys import argv
+
 if __name__ == '__main__':
-    """This gives acess to the database
-    and get the states from the database
     """
-db_connection = MySQLdb.connect(
-    host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-db_cursor = db_connection.cursor()
+    db_cursor = db_connect.cursor()
 
-db_cursor.execute("SELECT *from states")
+    db_cursor.execute("SELECT * FROM states")
 
-rows_selected = db_cursor.fetchall()
+    rows_selected = db_cursor.fetchall()
 
-for row in rows_selected:
-    print(row)
+    for row in rows_selected:
+        print(row)
