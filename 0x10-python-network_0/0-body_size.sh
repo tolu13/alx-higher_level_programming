24124dfc119cdb71db1004d32dc295b1d2eb7ce7
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-01-27 05:51:27 -0800

    net 0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This script  takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+curl -sI "$1" | awk '/Content-Length/{print $2}'
